5f1107b7e94da6877ba248ed5fd89ac7fef7f5d7
killuaJ2002 <kjcodes2002@gmail.com>
2025-12-11 00:47:38 +0530

    used the database for search queries

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from models import Product 
+from database import engine, session
+from sqlalchemy.orm import Session
+import database_models
 app = FastAPI()
+
+database_models.Base.metadata.create_all(bind=engine)
 
 @app.get('/')
 def greet():
@@ -10,15 +15,35 @@ products = [
     Product(id=1, name="smartphone", description="Midrange smartphone", price=28999, quantity=78),
     Product(id=34, name='bracelet', description="Men's accessories", price=499, quantity=146)
 ]
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally: 
+        db.close()
+
+def db__init():
+    db = session()
+    count = db.query(database_models.Product).count()
+
+    if count == 0:
+        for product in products: 
+            db.add(database_models.Product(**product.model_dump()))
+        db.commit()
+    
+db__init()
+
 @app.get('/products')
-def get_all_products():
-    return products
+def get_all_products(db: Session = Depends(get_db)): #Depends() is to inject a dependency
+    db_products = db.query(database_models.Product).all()
+    return db_products
 
 @app.get('/product/{id}')
-def get_product_by_id(id: int):
-    for product in products:
-        if product.id == id:
-            return product
+def get_product_by_id(id: int, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        return db_product
     return "Couldn't find product"
 
 @app.post('/product')
